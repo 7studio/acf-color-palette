222aadc8e0e18b9843989df9159f4cb6090b0a88
Moritz Lang <moritzlang@outlook.com>
2020-11-30 00:10:16 +0100

    feat: remove rgba conversion

--- assets/js/input.js
+++ assets/js/input.js
@@ -1,20 +1,40 @@
-( function( $ ) {
-    function initialize_field( $el ) {
-        var $input = $el.find( 'input[type="text"]' );
-        var $hidden = $el.find( 'input[type="hidden"]' );
+(function ($) {
+    function initialize_field($el) {
+        var $input = $el.find('input[type="text"]');
+        var $hidden = $el.find('input[type="hidden"]');
 
-        var change_hidden = function() {
+        var change_hidden = function () {
             // timeout is required to ensure the $input val is correct
-            setTimeout( function() {
+            setTimeout(function () {
                 var wpcp_val = $input.val();
-                var acf_val = sync_palettes[ get_rgba_notation( wpcp_val ) ];
+                var acf_val = wpcp_val;
                 var val = acf_val || wpcp_val;
 
-                acf.val( $hidden, val );
+                acf.val($hidden, val);
 
-                // display the user's color instead of the `wp-color-picker-alpha`'s one.'
-                $input.val( val );
-            }, 1 );
+                $input.val(get_user_value(val));
+            }, 1);
+        };
+
+        /**
+         * Returns the user's color instead of the `wp-color-picker-alpha`'s one.'
+         *
+         * @param {string} val - The wp-color-picker-alpha value
+         * @returns {string} - The user's value
+         */
+        var get_user_value = function (val) {
+            // Iterate over an array of strings, select the first elements that
+            // equalsIgnoreCase the 'matchString' value
+            var matchString = val.toLowerCase();
+            var userValue = null;
+            $.each(args.palettes, function (index, value) {
+                if (userValue == null && value.toLowerCase() === matchString) {
+                    userValue = value;
+                    return false;
+                }
+            });
+
+            return userValue;
         };
 
         var args = {
@@ -22,103 +42,24 @@
             palettes: false,
             hide: true,
             change: change_hidden,
-            clear: change_hidden
+            clear: change_hidden,
         };
 
         try {
-            args.palettes = JSON.parse( $input.attr( 'swp-acf-cp-palettes' ) );
+            args.palettes = JSON.parse($input.attr("swp-acf-cp-palettes"));
         } catch (error) {
-            console.log( error );
+            console.error(error);
         }
 
-        args = acf.apply_filters( 'swp_acf_color_palette_args', args, $el );
+        args = acf.apply_filters("swp_acf_color_palette_args", args, $el);
 
-        /**
-         * 1. Transform all colors to their `rgba` equivalents.
-         *    It's required because `wp-color-picker-alpha` doesn't work with
-         *    named colors and has some troubles în other cases:
-         * 2. Create an associative array linking the color picker's colors to the user's ones.
-         *    This array will allow to select the right value in the ACF field's choices.
-         */
-        var sync_palettes = Object.create(null);
-        args.palettes = args.palettes.map(function( c ) {
-            var rgba = get_rgba_notation( c );
-
-            sync_palettes[ rgba ] = c;
-
-            return rgba;
-        } );
-
-        /**
-         * `wp-color-picker-alpha` is an interesting enhancement of the `wpColorPicker`
-         * but it doesn't correctly handle named color at the beginning.
-         * This is why we must convert the value/notation before the color picker
-         * is started.
-         */
-        $input.val( get_rgba_notation( $input.val() ) );
+        $input.val(get_user_value($input.val()));
 
         // Iris initialisation
-        $input.wpColorPicker( args );
+        $input.wpColorPicker(args);
     }
 
-    /**
-     * Converts any colors (or almost) to their rgba equivalents.
-     *
-     * Many thanks to Oria for his brillant code :D
-     * https://stackoverflow.com/a/44655529/3356679
-     */
-    function get_rgba_notation( color ) {
-        if ( ! color ) {
-            return;
-        }
-
-        if ( color.toLowerCase() === 'transparent' ) {
-            return 'rgba(0, 0, 0, 0)';
-        }
-
-        if ( color[0] === '#' ) {
-            if (color.length < 7) {
-                // convert #RGB and #RGBA to #RRGGBB and #RRGGBBAA
-                color = '#' + color[1] + color[1] + color[2] + color[2] + color[3] + color[3] + (color.length > 4 ? color[4] + color[4] : '');
-            }
-
-            return 'rgba(' + parseInt( color.substr( 1, 2 ), 16) +
-                   ', ' + parseInt( color.substr( 3, 2 ), 16 ) +
-                   ', ' + parseInt( color.substr( 5, 2 ), 16 ) +
-                   ', ' + (color.length > 7 ? parseInt( color.substr( 7, 2 ), 16 ) / 255 : 1) + ')';
-        }
-
-        if ( color.indexOf( 'rgb' ) === -1 ) {
-            // convert named colors
-            var tmp_elem = document.body.appendChild( document.createElement( 'fictum' ) ); // intentionally use unknown tag to lower chances of css rule override with !important
-            var flag = 'rgb(1, 2, 3)'; // this flag tested on chrome 59, ff 53, ie9, ie10, ie11, edge 14
-
-            tmp_elem.style.color = flag;
-            if ( tmp_elem.style.color !== flag ) {
-                return; // color set failed - some monstrous css rule is probably taking over the color of our object
-            }
-
-            tmp_elem.style.color = color;
-
-            if ( tmp_elem.style.color === flag || tmp_elem.style.color === '' ) {
-                return; // color parse failed
-            }
-
-            color = getComputedStyle( tmp_elem ).color;
-
-            document.body.removeChild( tmp_elem );
-        }
-
-        if ( color.indexOf('rgb') === 0 ) {
-            if ( color.indexOf('rgba') === -1 ){
-                color += ',1'; // convert 'rgb(R,G,B)' to 'rgb(R,G,B)A' which looks awful but will pass the regxep below
-            }
-
-            return 'rgba(' + color.match( /[\.\d]+/g ).map(function( e ) { return e.replace( /^0\./, '.' ); } ).join( ', ' ) + ')';
-        }
-    }
-
-    if ( typeof acf.add_action !== 'undefined' ) {
+    if (typeof acf.add_action !== "undefined") {
         /**
          * ready append (ACF5)
          *
@@ -132,11 +73,11 @@
          * @param   $el (jQuery selection) the jQuery element which contains the ACF fields
          * @return  n/a
          */
-        acf.add_action( 'ready append', function( $el ) {
+        acf.add_action("ready append", function ($el) {
             // search $el for fields of type 'color_palette'
-            acf.get_fields( { type : 'color_palette' }, $el ).each( function() {
-                initialize_field( $( this ) );
-            } );
-        } );
+            acf.get_fields({ type: "color_palette" }, $el).each(function () {
+                initialize_field($(this));
+            });
+        });
     }
-} )( jQuery );
+})(jQuery);
